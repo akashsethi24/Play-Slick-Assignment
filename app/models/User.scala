d9b53ffd2d79af30fe8dcc6d46445997713a08e2
akashsethi24 <akashsethi@hotmail.com>
2016-03-08 16:07:26 +0530

    "All Forms Added in Services.Forms  object"

--- app/models/User.scala
+++ app/models/User.scala
@@ -5,3 +5,10 @@ package models
   */
 case class User(id:Int,email:String,password:String,mobile:Long,isAdmin:Boolean)
 
+case class Certificates(id:Int,userId:Int,name:String,description:String,year:Int)
+
+case class Languages(id:Int,userId:Int,name:String,fluency:String)
+
+case class Assignments(id:Int,userId:Int,name:String,date:String,remark:String,marks:Int)
+
+case class ProgrammingLanguages(id:Int,userId:Int,name:String,fluency:String)
